a766d1721eb0c9572a6e45382fd5469814135849
JK407 <1467511770@qq.com>
2024-03-07 14:40:20 +0800

    fix: update chainmaker.api && delete test api

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -13,16 +13,6 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 	server.AddRoutes(
 		[]rest.Route{
 			{
-				Method:  http.MethodGet,
-				Path:    "/users/id/:userId",
-				Handler: GetUserHandler(serverCtx),
-			},
-			{
-				Method:  http.MethodPost,
-				Path:    "/users/create",
-				Handler: CreateUserHandler(serverCtx),
-			},
-			{
 				Method:  http.MethodPost,
 				Path:    "/account/new",
 				Handler: NewAccountHandler(serverCtx),
