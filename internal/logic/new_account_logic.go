fbb26a29722703cae31ed39e080b5c2813f9c632
JK407 <1467511770@qq.com>
2024-03-07 14:49:40 +0800

    fix: add log

--- internal/logic/new_account_logic.go
+++ internal/logic/new_account_logic.go
@@ -33,13 +33,13 @@ func (l *NewAccountLogic) NewAccount(req *types.NewAccountRequest) (resp *types.
 	// 根据名称创建文件夹
 	dir := fmt.Sprintf("./accounts/%s", req.Name)
 	if err = os.MkdirAll(dir, 0755); err != nil {
-		// todo log
+		l.Logger.Errorf("MkdirAll error: ", err)
 		return &constants.MkdirAllError, nil
 	}
 
 	// 生成私钥
 	if err = l.keyService.GenerateKey(req.Algo, dir, req.Name); err != nil {
-		// todo log
+		l.Logger.Error("GenerateKey error: ", err)
 		return &constants.GenerateKeyError, nil
 	}
 	//prvKeyPath := filepath.Join(dir, fmt.Sprintf("%s.key", req.Name))
@@ -47,12 +47,12 @@ func (l *NewAccountLogic) NewAccount(req *types.NewAccountRequest) (resp *types.
 
 	// 生成公钥
 	if err = l.keyService.ExportPublicKey(prvKeyPath, dir, req.Name); err != nil {
-		// todo log
+		l.Logger.Errorf("ExportPublicKey error: ", err)
 		return &constants.ExportPublicKeyError, nil
 	}
 	//pubKeyPath := filepath.Join(dir, fmt.Sprintf("%s.pem", req.Name))
 	pubKeyPath := filepath.ToSlash(filepath.Join(dir, fmt.Sprintf("%s.pem", req.Name)))
-
+	l.Logger.Infof("success to create new account.")
 	r := constants.NewAccountSuccess.SetData(types.NewAccountData{
 		PrivKeyPath: prvKeyPath,
 		PubKeyPath:  pubKeyPath,
